53fdb2eabaf967ce0be63150d9abf898c771144e
Minuk Lee <zkvn1103@naver.com>
2023-02-04 00:08:31 +0900

    [Initial] OInjo_Shop
    Main

---
+++ OInjo_Shop/src/main/java/kr/idu/OInjo_Shop/OInjoShopApplication.java
@@ -0,0 +1,13 @@
+package kr.idu.OInjo_Shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OInjoShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OInjoShopApplication.class, args);
+	}
+
+}
