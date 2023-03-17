41b07666324e347c4a19774bc36c55f500a926eb
Minuk Lee <zkvn1103@naver.com>
2023-03-17 23:00:37 +0900

    [Init]BaseEntity, BaseTimeEntity
    작성 및 수정 시간 DB

--- OInjo_Shop/src/main/java/kr/idu/OInjo_Shop/entity/ItemEntity.java
+++ OInjo_Shop/src/main/java/kr/idu/OInjo_Shop/entity/ItemEntity.java
@@ -5,17 +5,43 @@ import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.*;
+import java.sql.Date;
 
 @Entity
-@Table(name="item")
+@Table(name="product")
 @Getter
 @Setter
 @ToString
 public class ItemEntity {
 
     @Id
-    @Column(name="item_id")
+    @Column(name="productId")
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id; // 상품 코드
+
+    @Column
+    private String productName;
+
+    @Column
+    private long price;
+
+    @Column
+    private String status;
+
+    @Column
+    private String contents;
+
+    @Column
+    private Date date;
+
+    @Column
+    private String opt1;
+
+    @Column
+    private String opt2;
+
+    @OneToOne
+    @Column
+    private category categoryId;
 
 }
