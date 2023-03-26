7b709b405327047b66eeb32358acdc2e9261f0eb
이민욱 <56884540+zkvn99@users.noreply.github.com>
2023-03-26 16:17:43 +0900

    [update]
    경로 수정

--- OInjo_Shop/src/main/java/kr/idu/OInjo_Shop/controller/MemberController.java
+++ OInjo_Shop/src/main/java/kr/idu/OInjo_Shop/controller/MemberController.java
@@ -64,7 +64,7 @@ public class MemberController {
         // List => DTO 객체가 담겨있음 [여러가지 데이터 가져올 때 List]
         model.addAttribute("memberList", memberDTOList);
         // model 객체로 list 담아감감
-        return "list";
+        return "memberlist";
     }
 
     @GetMapping("/member/{id}")
@@ -74,7 +74,7 @@ public class MemberController {
         MemberDTO memberDTO = memberService.findById(id);
         model.addAttribute("member", memberDTO);
         // html에서 member.memberEmail 형식으로 사용
-        return "detail";
+        return "memberdetail";
     }
  
     @GetMapping("/member/update")
