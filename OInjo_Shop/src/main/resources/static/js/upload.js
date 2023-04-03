cbfb894ac933ea93d5da7219cfa379579a8c1f6b
minwook <zkvn1103@naver.com>
2023-04-03 18:28:43 +0900

    front

--- OInjo_Shop/src/main/resources/static/js/upload.js
+++ OInjo_Shop/src/main/resources/static/js/upload.js
@@ -6,6 +6,33 @@ const productNameChangeFunction = () => {
 };
 productName.addEventListener("change", productNameChangeFunction);
 
-document.querySelector("#product-detail").addEventListener("click", () => {
-    console.log("bobobo");
-});
+// modal js
+const detailButton = document.querySelector(".detail-button");
+const modal = document.querySelector(".modal")
+const modalInput = document.querySelector(".modal-input");
+const modalResetButton = document.querySelector(".modal-reset-button");
+const modalSaveButton = document.querySelector(".modal-save-button");
+const detailLimit = document.querySelector(".detail-limit");
+
+detailButton.addEventListener("click", () => {
+    modal.style.display = "flex";
+})
+
+modalResetButton.addEventListener("click", () => {
+    modalInput.value = ""
+    detailLimit.innerText = `0/300`
+})
+
+modalSaveButton.addEventListener("click", () => {
+    modal.style.display = "none";
+})
+
+
+modalInput.addEventListener("change", () => {
+    if(modalInput.value.length > 300) {
+        alert("허용된 글자수를 넘었습니다.")
+    }
+    else {
+        detailLimit.innerText = `${modalInput.value.length}/300`
+    }
+})
