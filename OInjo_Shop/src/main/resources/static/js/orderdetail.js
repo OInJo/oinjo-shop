922e489f988a7bd606fad1107a11f61f9d404810
Dragonkeun <113826092+Dragonkeun@users.noreply.github.com>
2023-06-18 22:58:45 +0900

    Delete OInjo_Shop/src/main/resources directory

--- OInjo_Shop/src/main/resources/static/js/orderdetail.js
+++
@@ -1,2 +0,0 @@
-document.getElementById('select-calendar-start').value = new Date().toISOString().substring(0,10);
-document.getElementById('select-calendar-end').value = new Date().toISOString().substring(0,10);
