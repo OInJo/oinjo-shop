2335ec7a01c9eec5551b30a831ec91b2c41393af
minwook <zkvn1103@naver.com>
2023-06-24 02:28:04 +0900

    [EDIT]Mail
    이메일 인증 확인 구현

--- OInjo_Shop/src/main/java/kr/idu/OInjo_Shop/service/Mail/RegisterMail.java
+++ OInjo_Shop/src/main/java/kr/idu/OInjo_Shop/service/Mail/RegisterMail.java
@@ -92,7 +92,7 @@ public class RegisterMail implements MailServiceInter {
     // 메일 발송
     // sendSimpleMessage 의 매개변수로 들어온 to 는 곧 이메일 주소가 되고,
     // MimeMessage 객체 안에 내가 전송할 메일의 내용을 담는다.
-    // 그리고 bean 으로 등록해둔 javaMail 객체를 사용해서 이메일 send!!
+    // 그리고 bean 으로 등록해둔 javaMail 객체를 사용해서 이메일 send
     @Override
     public String sendSimpleMessage(String to) throws Exception {
 
