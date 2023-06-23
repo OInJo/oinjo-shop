70866c6753dfe8769f105e633b5e221003fd275c
minwook <zkvn1103@naver.com>
2023-06-23 22:07:28 +0900

    [EDIT]Item

--- OInjo_Shop/src/main/java/kr/idu/OInjo_Shop/controller/HomeController.java
+++ OInjo_Shop/src/main/java/kr/idu/OInjo_Shop/controller/HomeController.java
@@ -2,9 +2,13 @@ package kr.idu.OInjo_Shop.controller;
 
 import kr.idu.OInjo_Shop.dto.Item.ItemFormDTO;
 import kr.idu.OInjo_Shop.dto.Item.ItemImgDTO;
+import kr.idu.OInjo_Shop.dto.Item.Relation.BrandDTO;
+import kr.idu.OInjo_Shop.dto.Item.Relation.CategoryDTO;
+import kr.idu.OInjo_Shop.dto.Item.Relation.ColorDTO;
 import kr.idu.OInjo_Shop.dto.Item.Relation.SizeDTO;
 import kr.idu.OInjo_Shop.service.Item.ItemImgService;
 import kr.idu.OInjo_Shop.service.Item.ItemService;
+import kr.idu.OInjo_Shop.service.Item.RelationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,10 +22,22 @@ public class HomeController {
     // 기본페이지 요청 메서드
     private final ItemService itemService; // 아이템 및 아이템 이미지
     private final ItemImgService itemImgService; // 아이템 및 아이템 이미지
+    private final RelationService relationService; // 브랜드,카테고리,사이즈,컬러
+
     @GetMapping("/")
     public String index(Model model) {
-        List<ItemFormDTO> itemFormDTOList = itemService.findAllItem();
-        List<ItemImgDTO> itemImgDTOList = itemImgService.findAllItemImg();
+        List<ItemFormDTO> itemFormDTOList = itemService.findAllItem(); // 아이템 리스트
+        List<ItemImgDTO> itemImgDTOList = itemImgService.findAllItemImg(); // 아이템 이미지 리스트
+        List<BrandDTO> brandDTOList = relationService.findAllBrand(); // 브랜드 리스트
+        List<CategoryDTO> categoryDTOList = relationService.findAllCategory(); // 카테고리 리스트
+        List<ColorDTO> colorDTOList = relationService.findAllColor(); // 컬러 리스트
+        List<SizeDTO> sizeDTOList = relationService.findAllSize(); // 사이즈 리스트
+        model.addAttribute("itemFormDTOList", itemFormDTOList); // 아이템 리스트 받아오기
+        model.addAttribute("itemImgDTOList", itemImgDTOList); // 아이템 이미지 리스트 받아오기
+        model.addAttribute("brandList", brandDTOList); // 브랜드 리스트 받아오기
+        model.addAttribute("categoryList", categoryDTOList); // 카테고리 리스트 받아오기
+        model.addAttribute("colorList", colorDTOList); // 컬러 리스트 받아오기
+        model.addAttribute("sizeList", sizeDTOList); // 사이즈 리스트 받아오기
         return "index"; // templates/index.html
     }
     @GetMapping("/email")
