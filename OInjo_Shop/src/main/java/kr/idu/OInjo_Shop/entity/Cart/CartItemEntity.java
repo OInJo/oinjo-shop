dd83a26135e7ffbb9b1c26371e13e5a173e139f6
이경호 <rud0503@gmail.com>
2023-06-26 00:23:05 +0900

    Fix:CartEntity

--- OInjo_Shop/src/main/java/kr/idu/OInjo_Shop/entity/Cart/CartItemEntity.java
+++ OInjo_Shop/src/main/java/kr/idu/OInjo_Shop/entity/Cart/CartItemEntity.java
@@ -18,7 +18,6 @@ import java.time.LocalDate;
 public class CartItemEntity {
 
     @Id
-    @Column(name="cart_items")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
