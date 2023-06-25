8c8dd29f7062a9706fc695421cc067d5ee00a149
Dragonkeun <dragonkeun7@gmail.com>
2023-06-25 19:23:23 +0900

    [Feat][Front]아이디찾기, 비밀번호찾기 구현, 아이디찾기 전화번호 유효성검사 구현, 비밀번호 찾기 구현 필요

--- OInjo_Shop/src/main/resources/static/js/findid.js
+++ OInjo_Shop/src/main/resources/static/js/findid.js
@@ -1,22 +1,16 @@
-// jQuery(요즘은 잘 쓰지않음), jQuery cdn 받아와야함
-// let id = $("#id");
-// let btn = $("#btn");
-// $(btn).on("click", function () {
-//   if ($(id).val() == "") {
-//     $(id).next("label").addClass("warning");
-//   }
-// });
-
-const email = document.querySelector("#email");
-const emailInputLabel = document.querySelector(".email-input-label");
-const emailChange = () => {
-  if (email.value != "") {
-    emailInputLabel.style.fontSize = "15px";
-    emailInputLabel.style.top = "480px";
+const phoneNumberInput = document.querySelector(".phone-number-input");
+const phoneNumberCertificationErrorMessage = document.querySelector(".phone-number-certification-error-message")
+phoneNumberInput.addEventListener("input", (event) => {
+  const phoneNumber = event.target.value;
+  if (
+    /^\d+$/.test(phoneNumber) &&
+    phoneNumber.length == 11 &&
+    phoneNumber.charAt(0) == "0"
+  ) {
+    phoneNumberInput.style.border = "";
+    phoneNumberCertificationErrorMessage.style.display = "none"
+  } else if(phoneNumber.length > 0){
+    phoneNumberInput.style.border = "1px solid red";
+    phoneNumberCertificationErrorMessage.style.display = "block"
   }
-  else {
-    emailInputLabel.style.fontSize = "22px";
-    emailInputLabel.style.top = "";
-  }
-};
-email.addEventListener("change", emailChange);
+});
