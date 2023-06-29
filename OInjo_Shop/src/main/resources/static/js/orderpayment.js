6b8881d70b4cb49aa318889392756aaff3d61598
jeonghyeon <dkdlzndi78@gmail.com>
2023-06-29 22:44:01 +0900

    [Feat][Front]배송지 변경 팝업창 수정

--- OInjo_Shop/src/main/resources/static/js/orderpayment.js
+++ OInjo_Shop/src/main/resources/static/js/orderpayment.js
@@ -1,47 +1,24 @@
-function DaumPostcode() {
-    new daum.Postcode({
-        oncomplete: function(data) {
-            // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분.
+function address_popup() {
+    var popup = window.open('orderpaymentpopup.html', '배송지', 'width=600, height=700, left=0, top=0');
 
-            // 도로명 주소의 노출 규칙에 따라 주소를 표시한다.
-            // 내려오는 변수가 값이 없는 경우엔 공백('')값을 가지므로, 이를 참고하여 분기 한다.
-            var roadAddr = data.roadAddress; // 도로명 주소 변수
-            var extraRoadAddr = ''; // 참고 항목 변수
+    '<p><button onclick="window.close()">닫기</button>';
+  // 팝업 창의 document 객체를 사용하여 내용 삽입
 
-            // 법정동명이 있을 경우 추가한다. (법정리는 제외)
-            // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
-            if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
-                extraRoadAddr += data.bname;
-            }
-            // 건물명이 있고, 공동주택일 경우 추가한다.
-            if(data.buildingName !== '' && data.apartment === 'Y'){
-               extraRoadAddr += (extraRoadAddr !== '' ? ', ' + data.buildingName : data.buildingName);
-            }
-            // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
-            if(extraRoadAddr !== ''){
-                extraRoadAddr = ' (' + extraRoadAddr + ')';
-            }
-
-            // 우편번호와 주소 정보를 해당 필드에 넣는다.
-            document.getElementById('user-postcode').value = data.zonecode;
-            document.getElementById("user-road-address").value = roadAddr;
-            document.getElementById("user-jibun-address").value = data.jibunAddress;
-
-            var guideTextBox = document.getElementById("guide");
-            // 사용자가 '선택 안함'을 클릭한 경우, 예상 주소라는 표시를 해준다.
-            if(data.autoRoadAddress) {
-                var expRoadAddr = data.autoRoadAddress + extraRoadAddr;
-                guideTextBox.innerHTML = '(예상 도로명 주소 : ' + expRoadAddr + ')';
-                guideTextBox.style.display = 'block';
-
-            } else if(data.autoJibunAddress) {
-                var expJibunAddr = data.autoJibunAddress;
-                guideTextBox.innerHTML = '(예상 지번 주소 : ' + expJibunAddr + ')';
-                guideTextBox.style.display = 'block';
-            } else {
-                guideTextBox.innerHTML = '';
-                guideTextBox.style.display = 'none';
-            }
-        }
-    }).open();
 }
+
+// 데이터 수신 이벤트 핸들러
+window.addEventListener("message", function(event) {
+    var receivedData = event.data;
+    
+    // 전달된 데이터 사용
+    var postcode = receivedData.postcode;
+    var roadAddress = receivedData.roadAddress;
+    var jibunAddress = receivedData.jibunAddress;
+    var detailAddress = receivedData.detailAddress;
+    var extraAddress = receivedData.extraAddress;
+    
+    // 주소 정보를 부모 HTML의 특정 요소에 표시
+    var deliveryDiv = document.querySelector(".go-delivery");
+    deliveryDiv.innerHTML = postcode + " " + roadAddress + " " + jibunAddress + " " + detailAddress + " " + extraAddress;
+    deliveryDiv.style.marginLeft = "125px";
+});
