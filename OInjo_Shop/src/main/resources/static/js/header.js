d0345e97748d1674d09d5a797628726097cea2ff
Dragonkeun <dragonkeun7@gmail.com>
2023-07-07 20:29:16 +0900

    [Feat][Front]폰트 적용 완료, index header 디자인 수정, 모바일 반응형 웹으로 구현

--- OInjo_Shop/src/main/resources/static/js/header.js
+++ OInjo_Shop/src/main/resources/static/js/header.js
@@ -3,8 +3,15 @@ const loginCheck = document.querySelector(".login-check");
 const updateLink = document.querySelector(".update-link");
 const logoutLink = document.querySelector(".logout-link");
 const loginLink = document.querySelector(".login-link");
+const adminLink = document.querySelector(".admin-link");
 loginCheck.style.display = "none";
 window.onload = () => {
+  if(loginCheck.textContent.includes("admin")){
+    adminLink.style.display = "flex";
+  }
+  else{
+    adminLink.style.display = "none"
+  }
   if(loginCheck.textContent == "") {
     updateLink.style.display = "none"
     logoutLink.style.display = "none"
