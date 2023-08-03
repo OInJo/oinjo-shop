e7ee7ad4d257caafd1c31847fff9ed17a43fce6c
Dragonkeun <dragonkeun7@gmail.com>
2023-08-04 06:16:16 +0900

    [Feature][Front]장바구니 수량 변경되도록 구현

--- OInjo_Shop/src/main/resources/static/js/cart/cart.js
+++ OInjo_Shop/src/main/resources/static/js/cart/cart.js
@@ -11,86 +11,95 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
-// 장바구니의 상품 수량개수를 수정하는 소스인데 현재 작동X
+// 장바구니의 상품 수량개수를 수정하는 소스
 // +버튼 클릭 시 이전 수량에서 1을 더하고, -버튼 클릭 시 이전 수량에서 1을 뺌. 수량의 최소값은 1
 // 서버에 requestUrl에 PUT 메소드로 id, itemId, count 3개의 값을 보내줌
 const memberId = parseInt(document.querySelector(".member-id").value);
-const cartItemId = parseInt(document.querySelector(".cart-item-id").value);
-const faMinus = document.querySelector(".fa-minus");
-const faPlus = document.querySelector(".fa-plus");
-let cartContentCounting = parseInt(
-  document.querySelector(".cart-content-counting").textContent
-); // 문자열을 숫자로 변환
+const cartItemId = document.querySelectorAll(".cart-item-id");
+const faMinus = document.querySelectorAll(".fa-minus");
+const faPlus = document.querySelectorAll(".fa-plus");
+let cartContentCounting = document.querySelectorAll(".cart-content-counting"); //문자열을 숫자로 변환
+for (let i = 0; i < faMinus.length; i++) {
+  const requestUrl = `/member/${memberId}/cart/${cartItemId[i].value}/update`;
 
-const requestUrl = `/member/${memberId}/cart/${cartItemId}/update`;
+  const cartContentPriceWon = document.querySelectorAll(
+    ".cart-content-price-won"
+  );
+  const cartContentTotalPriceWon = document.querySelectorAll(
+    ".cart-content-total-price-won"
+  );
 
-faMinus.addEventListener("click", () => {
-  if (cartContentCounting > 1) {
-    cartContentCounting--;
+  //마이너스 클릭 시, 수량이 1이 아니라면 1씩 빼줌, 1보다 더 작아질 수는 없음
+  faMinus[i].addEventListener("click", () => {
+    if (parseInt(cartContentCounting[i].textContent) > 1) {
+      cartContentCounting[i].textContent =
+        parseInt(cartContentCounting[i].textContent) - 1;
+
+      // PUT 메서드를 사용하여 요청을 보냄
+      fetch(
+        `${requestUrl}?id=${memberId}&itemId=${cartItemId[i].value}&count=${cartContentCounting[i].textContent}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            id: memberId,
+            itemId: cartItemId[i].value,
+            count: cartContentCounting[i].textContent,
+          }),
+        }
+      )
+        .then((response) => {
+          console.log(response);
+          const cartContentCountingElement = document.querySelectorAll(
+            ".cart-content-counting"
+          );
+          cartContentCountingElement[i].textContent =
+            cartContentCounting[i].textContent;
+          cartContentTotalPriceWon[i].textContent =
+            cartContentPriceWon[i].textContent *
+            cartContentCounting[i].textContent;
+        })
+        .catch((error) => {
+          console.error("오류 발생:", error);
+        });
+    }
+  });
+
+  //플러스 버튼 클릭 시 수량 1개 더함
+  faPlus[i].addEventListener("click", () => {
+    cartContentCounting[i].textContent =
+      parseInt(cartContentCounting[i].textContent) + 1;
 
     // PUT 메서드를 사용하여 요청을 보냄
-    fetch(requestUrl, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        id:memberId,
-        itemId:cartItemId,
-        count: cartContentCounting,
-      }),
-    })
+    fetch(
+      `${requestUrl}?id=${memberId}&itemId=${cartItemId[i].value}&count=${cartContentCounting[i].textContent}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id: memberId,
+          itemId: cartItemId[i].value,
+          count: cartContentCounting[i].textContent,
+        }),
+      }
+    )
       .then((response) => {
-        if (response.ok) {
-          // 수정 성공
-          console.log("수정 성공");
-          updateCartContentCount();
-        } else {
-          // 수정 실패
-          console.log("수정 실패");
-        }
-        console.log(response)
+        console.log(response);
+        const cartContentCountingElement = document.querySelectorAll(
+          ".cart-content-counting"
+        );
+        cartContentCountingElement[i].textContent =
+          cartContentCounting[i].textContent;
+        cartContentTotalPriceWon[i].textContent =
+          cartContentPriceWon[i].textContent *
+          cartContentCounting[i].textContent;
       })
       .catch((error) => {
         console.error("오류 발생:", error);
       });
-  }
-});
-
-faPlus.addEventListener("click", () => {
-  cartContentCounting++;
-
-  // PUT 메서드를 사용하여 요청을 보냄
-  fetch(requestUrl, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-        id:memberId,
-        itemId:cartItemId,
-        count: cartContentCounting,
-    }),
-  })
-    .then((response) => {
-      if (response.ok) {
-        // 수정 성공
-        console.log("수정 성공");
-        updateCartContentCount();
-      } else {
-        // 수정 실패
-        console.log("수정 실패");
-      }
-    })
-    .catch((error) => {
-      console.error("오류 발생:", error);
-    });
-});
-
-//사용자 화면에 변경된 수량을 보여주도록 구현
-function updateCartContentCount() {
-  const cartContentCountingElement = document.querySelector(
-    ".cart-content-counting"
-  );
-  cartContentCountingElement.textContent = cartContentCounting; // 수정된 수량을 화면에 반영
+  });
 }
